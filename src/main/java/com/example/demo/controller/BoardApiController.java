1b17f27b4e1cf4368ced83aaca420b83d937ba33
HONGSEONGCHAN <seongchan_@naver.com>
2021-08-23 13:33:31 +0900

    Make REST API _1

---
+++ src/main/java/com/example/demo/controller/BoardApiController.java
@@ -0,0 +1,68 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Board;
+import com.example.demo.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.util.StringUtils;
+
+import java.util.List;
+
+//For REST API
+@RestController
+@RequestMapping("/api")
+public class BoardApiController {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+//    Select for All
+//    Add Search using title
+//    Add Search using title or Content
+//    Content Param => @RequestParam(required = false, defaultValue = ""
+    @GetMapping("/boards")
+    List<Board> all(@RequestParam(required = false, defaultValue = "") String title) {
+        if (StringUtils.isEmpty(title)) {
+//        if (StringUtils.isEmpty(title) && StringUtils.isEmpty(content)) {
+            return boardRepository.findAll();
+        } else {
+//            return boardRepository.findByTitle(title);
+//            return boardRepository.findByTitleOrContent(title, content);
+            return boardRepository.findByTitleContaining(title);
+        }
+    }
+
+//    Insert
+    @PostMapping("/boards")
+    Board newBoard(@RequestBody Board newBoard) {
+        return boardRepository.save(newBoard);
+    }
+
+//    Select for one
+//    ID 있으면 값 가져오고 없으면 Null 값
+    @GetMapping("/boards/{id}")
+    Board one(@PathVariable Long id) {
+        return boardRepository.findById(id).orElse(null);
+    }
+
+//    Update
+    @PutMapping("/boards/{id}")
+    Board replaceBoard(@RequestBody Board newBoard, @PathVariable Long id) {
+        return boardRepository.findById(id)
+                .map(board -> {
+                    board.setTitle(newBoard.getTitle());
+                    board.setContent(newBoard.getContent());
+                    return boardRepository.save(board);
+                })
+                .orElseGet(() -> {
+                    newBoard.setId(id);
+                    return boardRepository.save(newBoard);
+                });
+    }
+
+//    Delete
+    @DeleteMapping("/boards/{id}")
+    void deleteBoard(@PathVariable Long id) {
+        boardRepository.deleteById(id);
+    }
+}
--- src/main/java/com/example/demo/repository/BoardRepository.java
+++ src/main/java/com/example/demo/repository/BoardRepository.java
@@ -3,5 +3,14 @@ package com.example.demo.repository;
 import com.example.demo.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+//    제목으로 검색하기 메소드
+    List<Board> findByTitle(String title);
+//    제목 또는 내용으로 검색하기 메소드
+    List<Board> findByTitleOrContent(String title, String content);
+//    검색 단어가 포함된 제목 검색하기 메소드
+    List<Board> findByTitleContaining(String title);
 }
