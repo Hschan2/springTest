68d9795aa61a48d37a05795044d67f7d9e51c6f7
HONGSEONGCHAN <seongchan_@naver.com>
2021-08-24 13:48:09 +0900

    Makes Pagination

--- src/main/java/com/example/demo/controller/BoardController.java
+++ src/main/java/com/example/demo/controller/BoardController.java
@@ -4,6 +4,9 @@ import com.example.demo.model.Board;
 import com.example.demo.repository.BoardRepository;
 import com.example.demo.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -13,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @Controller
 @RequestMapping("/board")
@@ -26,8 +28,15 @@ public class BoardController {
     private BoardValidator boardValidator;
 
     @GetMapping("/list")
-    public String list(Model model) {
-        List<Board> boards = boardRepository.findAll(); // 데이터 베이스의 게시판 모두 가져오기
+    public String list(Model model, @PageableDefault(size = 2) Pageable pageable) {
+//        List<Board> boards = boardRepository.findAll(); // 데이터 베이스의 게시판 모두 가져오기
+        Page<Board> boards = boardRepository.findAll(pageable);
+
+        int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
+        int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
+
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         model.addAttribute("boards", boards); // 모델에 모든 데이터 담기 (타임리프에 출력)
         return "board/list";
     }
